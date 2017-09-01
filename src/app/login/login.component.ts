605fdf53fc0ee03fccbf2ff01758e534f1eb250f
Luis Gonzalez <lx.mw001@gmail.com>
2017-08-31 23:20:13 -0500

    Basic login page

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+// constructor(private router:Router, private user:UserService) { }
+	constructor(private router:Router) { }
+
+	ngOnInit() {
+		console.log('hit');
+	}
+
+	loginUser(e) {
+		e.preventDefault();
+		console.log(e);
+		var username = e.target.elements[0].value;
+		var password = e.target.elements[1].value;
+		
+		if(username == 'admin' && password == 'admin') {
+	  // 		this.user.setUserLoggedIn();
+			// this.router.navigate(['dashboard']);
+		}
+	}
+
+}
